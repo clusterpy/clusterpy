b113e2ee4c97cbfb4300b22daa3209e8b8580ed3
Sergio Botero <sergiobuj@gmail.com>
2013-08-22 17:20:09 -0500

    Add test structure for clustering algorithms

---
+++ clusterpy/tests/test_clustering.py
@@ -0,0 +1,17 @@
+"""
+Testing clustering algorithms in Clusterpy
+** All the following tests take considerable time to complete **
+"""
+
+from unittest import TestCase
+
+class TestClusteringAlgorithms(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_arisel(self):
+        assert False
+
+    def tearDown(self):
+        pass
