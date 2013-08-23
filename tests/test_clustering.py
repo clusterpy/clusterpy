d2c1f313929e18b037a7d50bf7810d8ef0544745
Sergio Botero <sergiobuj@gmail.com>
2013-08-23 13:40:39 -0500

    Renaming and add contiguity WIP test

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -1,16 +1,19 @@
 """
-Testing clustering algorithms in Clusterpy
+Testing clustering algorithms in Clusterpy -Arisel-
 ** All the following tests take considerable time to complete **
 """
 
 from unittest import TestCase
 import clusterpy
+from numpy.random import seed as make_static_random
 
-map_type = 'n529'
-sample_input_path = "./sample_input_data/" + map_type
-sample_output_path = "./sample_output/" + map_type
+map_type = 'n100'
+into_regions = 10
 
-class TestClusteringAlgorithms(TestCase):
+sample_input_path = "clusterpy/data_examples/" + map_type
+sample_output_path = "tests/sample_output/" + map_type
+
+class TestArisel(TestCase):
     def setUp(self):
         self.map_instance = clusterpy.importArcData(sample_input_path)
 
@@ -18,12 +21,40 @@ class TestClusteringAlgorithms(TestCase):
         # Remove generated output/Arc data
         pass
 
-    def test_arisel(self):
-        output_file = sample_output_path + 'arisel'
-        into_regions = 53
-        self.map_instance.cluster('arisel',
-                                  ['SAR1'],
-                                  into_regions,
-                                  dissolve = 1,
+    def test_arisel_never_breaks_contiguity(self):
+        """
+        Tests that the output regions never break the contiguity constraint.
+        """
+        instance = self.map_instance
+
+        make_static_random(10)
+        instance.cluster('arisel', ['SAR1'],
+                                  into_regions, dissolve = 1,
                                   inits = 20)
+
+        exp_name = instance.fieldNames[-1]
+        clustering_results = instance.outputCluster[exp_name]
+        final_region_assignment = clustering_results['r2a']
+
+        # Check contiguity of 'final_region_assignment'
+
         assert False
+
+    def test_arisel_gives_at_least_same_obj_func(self):
+        """
+        Tests that the objective function is at least the same, but not worse.
+        """
+        instance = self.map_instance
+
+        make_static_random(10)
+        initial_obj_func = float(90.1868744781) # Using a seed of 10
+
+        instance.cluster('arisel', ['SAR1'],
+                                  into_regions, dissolve = 1,
+                                  inits = 20)
+
+        exp_name = instance.fieldNames[-1]
+        clustering_results = instance.outputCluster[exp_name]
+        final_obj_func = clustering_results['objectiveFunction']
+
+        assert initial_obj_func >= final_obj_func
